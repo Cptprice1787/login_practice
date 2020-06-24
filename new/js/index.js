9cb0b624c86ff1c1819925aec3659fc9069cdcda
Cptprice1787 <646236235@qq.com>
2020-06-24 17:35:18 +0800

    Add files via upload

--- new/js/index.js
+++ new/js/index.js
@@ -22,6 +22,7 @@ for (let i = 0;i < buttonArr.length;i++) {
 }
 
 //登陆判断模拟
+/*
 var ac = document.getElementById("form1");
 let tu = "admin", tp = 123456;
 function log_in () {
@@ -36,3 +37,61 @@ function log_in () {
     alert("账号或密码错误，请重新输入");
   }
 }
+*/
+
+//正则账号密码格式识别
+function log_in () {
+  var username = document.getElementsByName("username1")[0].value;
+  var password = document.getElementsByName("password1")[0].value;
+  var t = new Array(true, true, true, true);
+  var ac = document.getElementById("form1");
+
+  var a = b = c = d = "\n";
+  var regu = new RegExp("^[A-z]([A-z]|[0-9]){4,8}$");
+  var regp = new RegExp("^([A-z]|[0-9]|[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？]){6,12}$");
+  var regpI = new RegExp("[A-Z]");
+  var regpS = new RegExp("[`~!@#$^&*()=|{}':;',\\[\\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？]");
+  var us = regu.test(username);
+  var ps = regp.test(password);
+  var psI = regpI.test(password);
+  var psS = regpS.test(password);
+  var match = new Array(us, ps, psI, psS)
+  if (match[0] == false)
+    a = "账户必须以大写字母开头且包含5-8位字母或数字";
+  if (match[1] == false)
+    b = "密码只能包含6-12位英文字母、数字和特殊符号";
+  if (match[2] == false)
+    c = "密码须至少有一个大写字母";
+  if (match[3] == false)
+    d = "密码须至少有一个特殊符号";
+  if (match.toString() == t.toString()) {
+    ac.action = "../new/receive.htm";
+    ac.submit();
+  }
+  else
+    alert(a + b + c + d);
+}
+
+
+
+
+
+
+/**
+ * function:校验输入特殊字符"<>/""
+ * @keyCode:字符串 string
+ */
+/*const checkSpecificKey = function (keyCode) {
+    let specialKey = "<>/";
+    let realkey = String.fromCharCode(keyCode);
+    let flg = false;
+    var flag = /[<>/]/
+    flg = flag.test(keyCode);
+    if (flg) {
+      toast("请勿输入特殊字符<>/", 2000);
+      keyCode = keyCode.replace(flag, '"');
+      return false;
+    }
+    return true;
+  }
+  */
